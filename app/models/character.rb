c22c9fc6236394f891b6658141d1a2c29762902f
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-04 16:58:10 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,12 +1,13 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
+  belongs_to :network, through :show
 
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
 
-  # def build_network
-  #   self.build_show(name: )
-  # end
+  def build_network
+    self.build_show(name: )
+    end
 end
